20acb6cd40ac1e7420dcd76298c71c432d01d053
IngNex <ingnexmaicol14@gmail.com>
2022-09-22 14:07:04 -0500

    add details object screen

---
+++ lib/details_object_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailsObjectScreen extends StatefulWidget {
+  const DetailsObjectScreen({super.key});
+
+  @override
+  State<DetailsObjectScreen> createState() => _DetailsObjectScreenState();
+}
+
+class _DetailsObjectScreenState extends State<DetailsObjectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
